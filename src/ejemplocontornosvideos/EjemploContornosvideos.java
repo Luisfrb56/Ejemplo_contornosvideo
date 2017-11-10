34d2783b35a194114e3c084421cee702cd70f071
adm <adm@LAPTOP-EQL1FQIM>
2017-11-10 09:30:44 +0100

    ejemplocontornos1

---
+++ src/ejemplocontornosvideos/EjemploContornosvideos.java
@@ -0,0 +1,12 @@
+
+package ejemplocontornosvideos;
+
+
+public class EjemploContornosvideos {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Pantalla");
+    }
+    
+}
